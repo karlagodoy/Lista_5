da8a2f6873510f745e76e93b8fab657539223218
karlagodoy <49796092+karlagodoy@users.noreply.github.com>
2019-05-11 04:55:59 -0300

    Update Lista_5_Karla_Godoy.R

--- Lista_5_Karla_Godoy.R
+++ Lista_5_Karla_Godoy.R
@@ -4,7 +4,7 @@
 
 # Respostas da lista 1----
 
-# Questão 5----
+# QuestÃ£o 5----
 
 require(tidyverse)
 require(magrittr)
@@ -34,7 +34,7 @@ class (Tamanho) #character
 
 # B----
 
-#Empresas do setor do Comércio 
+#Empresas do setor do ComÃ©rcio 
 
 Q5Comercio <- Q_5 %>% filter(Setor=="C")
 
@@ -44,7 +44,7 @@ mean (Q5Comercio$Meses) #7.142857
 
 median(Q5Comercio$Meses) #8
 
-#Empresas do setor da Indústria
+#Empresas do setor da IndÃºstria
 
 Q5Industria <- Q_5 %>% filter(Setor=="I")
 
@@ -52,7 +52,7 @@ mean (Q5Industria$Meses) #4.625
 
 median(Q5Industria$Meses) #4.5
 
-# as empresas do Comercio apresentam média de meses em crescimento maior que as empresas na  área de Industria.
+# as empresas do Comercio apresentam mÃ©dia de meses em crescimento maior que as empresas na  Ã¡rea de Industria.
 
 # C----
 
@@ -64,7 +64,7 @@ sd(Q5Comercio$Meses) #1.46385
 
 sd(Q5Industria$Meses) #1.06066
 
-#O grupo mais homogêneo é o grupo formado pelas empresas do ramo da Industria.
+#O grupo mais homogÃªneo Ã© o grupo formado pelas empresas do ramo da Industria.
 
 # D----
 
@@ -72,8 +72,8 @@ summary (Q_5$Meses)
 
 #  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #  3.0     4.5     6.0     5.8     7.0     9.0 
-# A medida descritiva que fornece a informação é o 1º P-  quantil (1st Qu).
-# O número de meses apresentando crescimento de no máximo 4,5.
+# A medida descritiva que fornece a informaÃ§Ã£o Ã© o 1Âº P-  quantil (1st Qu).
+# O nÃºmero de meses apresentando crescimento de no mÃ¡ximo 4,5.
 
 
 # E----
@@ -108,8 +108,10 @@ median(Q5EmpresasG$Meses) #6
 
 sd(Q5EmpresasG$Meses) #2.309401
 
-## VER RESPOSTA DO PROFESSOR 
-
+## A partir dos resultados obtidos conclui-se que não parece
+#haver relação significativa entre o tamanho da empresa e o tempo de meses
+#com crescimento. Os tempos médios e medianos são bastante
+#semelhantes nos 3 grupos.
 
 #6----
 
@@ -129,8 +131,9 @@ sd(Q_6$Investimento)# 4.184628
 
 
 
-## Nenhuma das cidades receberá o programa, visto que nenhuma das cidades atende aos requisitos.
-## Isso acontece porque todos os investimentos estão proximos da média.
+## Nenhuma das cidades receberÃ¡ o programa, visto que nenhuma das cidades atende aos requisitos, dado que as codificações obtidas
+## nas cidades são superiores a 8,83.
+## Isso acontece porque todos os investimentos estÃ£o proximos da mÃ©dia.
 
 #C
 
@@ -138,7 +141,11 @@ mean(Q_6$Investimento)- 2*sd(Q_6$Investimento) #8.830744
 
 mean(Q_6$Investimento)+ 2*sd(Q_6$Investimento) #25.56926
 
-## Apenas a cidade A ficará de fora. 
+## Da relaçãoo das cidades, apenas a cidade, cujo investimento vale 26, não
+## contribuirá para o cálculo do investimento básico. Com a retirada da cidade A percebe-se que o investimento 
+## básico é menor que a média de investimentos realizados pelas  cidades remanecentes. Isso acontece porque a cidade A
+## tem um investimento muito superior e sua retirada faz com que a média caia . 
+
 
 #7 ----
 
@@ -146,36 +153,41 @@ Ind<-  c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 
 A <- c(55, 2, 13, 11, 23, 2, 15, 12, 14, 28, 12, 45, 19, 30, 16, 12, 7, 13, 1, 7)
 
-B <- c(20, 7, 6, 5, 3, 25, 5, 3, 3, 10, 8, 5, 1, 35, 9, 8, 12, 2, 26)
+B <- c(20, 7, 6, 5, 3, 25, 5, 3, 3, 10, 8, 5, 1, 35, 9, 8, 12, 2, 26, NA)
 
-Q_7 <- data.frame (Ind, A, B)
+Q_7 <- data.frame (A, B)
 
 
 #A
 
-# Estímulo A
+# EstÃ­mulo A
 
 mean (Q_7$A) #16.85
 
 median(Q_7$A) #13
 
+var(Q_7$A) # 190.5553
+
 sd(Q_7$A) #13.80418
 
-# Estímulo B
+# EstÃ­mulo B
 
 mean (B) #10.15789
 
 median(B) #7
 
+var(B) # 89.47368
+
 sd(B) #9.459053
 
-## O estímulo B tem o tempo o menor tempo de reação, tem média 
-## e mediana menor que o estímulo A, e ainda apresenta uma amostra mais homogênea. 
+## O estÃ­mulo B tem o tempo o menor tempo de reaÃ§Ã£o, tem mÃ©dia 
+## e mediana menor que o estÃ­mulo A, e ainda apresenta uma amostra mais homogÃªnea. 
 
 #B
 
 require(ggplot2)
-boxplot(A,B)
+boxplot(Q_7)
+
 
 #8----
 
@@ -185,7 +197,7 @@ renda_sm <- c(12, 16, 18, 20, 28, 30, 40, 48, 50, 54)
 
 saude_perc <- c(7.2, 7.4, 7, 6.5, 6.6, 6.7, 6, 5.6, 6, 5.5)
 
-Q_8 <- data.frame (fam, renda_sm, saude_perc)
+Q_8 <- data.frame (renda_sm, saude_perc)
 
 
 #A
@@ -206,16 +218,20 @@ sd(Q_8$saude_perc) #0.6570134
 
 #Covariancia 
 
+cov(Q_8) #  -9.533333 
 
-#Correlação
+#CorrelaÃ§Ã£o
 
 cor.test(Q_8$renda_sm, Q_8$saude_perc)
-
-cor(Q_8$renda_sm, Q_8$saude_perc)
 
 #-0.9404625
 
 #B
+
+## Pelos valores calculados, especialmente a covariância e o coeficiente de
+## correlação linear, conclui-se que as duas variáveis têm associação negativa
+## e forte. Dessa foma, ao passo em que  a renda em salários mínimos aumenta, o
+## percentual de gasto com saúde diminui. 
 
 
 #9----
@@ -234,56 +250,177 @@ cor.test(Q_9$P1, Q_9$P2)
 
 #0.8301592
 
+#B 
+
+## Conforme observado com o coeficiente de correlação linear de Pearson, 
+## há uma relação linear positiva forte entre a nota na P1 e a nota na P2.
+
+
 # Lista 2----
 
-#Questão5----
+#QuestÃ£o5----
 
-Pesquisa <- c(1:1000)
+# 1000 eleitores 
+# 620 jamais votariam no candidato (sucesso=1)
+# 380 não responderam ou votariam no candidato (fracasso=0)
 
-
-Respostas <- c(1:620, replace=N, 621:1000, replace=S)
-
-
-Pesquisa <- list(N,Sim)
+votos <- rep (c (1,0), times= c(620,380))
 
 # A
 
-(((620*1)+(380*0))/1000)
+mean(votos)
 
 # 0.62
 
 # B
 
-# Desvio padrão
-sd(Pesquisa$Nao)
-# ou
-y %>% var %>% sqrt
+sd(votos)
 
-# Questão 6----
+# 0.4856293
+
+#C
+
+t.test(votos)
+
+# 0,62
+## Considerando^pnossa estimativa pontual igual a 0,62 e o coeficiente
+## de confiança de 95%, existe 95% de confiança de que o intervalo 
+## calculado apresenta o valor da média populacional.
+
+
+# QuestÃ£o 6----
 
 #A
 
+((1.96^2)*0.5*(1-0.5))/(0.05^2)
+
+#384.16
+
 #B
 
+((1.96^2)*0.5*(1-0.5))/(0.02^2)
+
+#2401
+
 #C
+
+((1.96/0.02)^2)*0.1875
+
+# 1801. A amostra seria reduzida em 600.
 
 #D
+# 564 votaram no candidato.(sucesso=1)
+#1837 votaram não votaram em outros.(fracasso=0)
 
-#Questão11
+votos_Q6 <- rep (c (1,0), times= c(524,1837))
 
-#A
+mean(votos_Q6)
+#0.2219399
+
+sd(votos_Q6)
+
+#0.4156389
+
+Oy<- (0.4156389/sqrt(2401))
+
+(mean(votos_Q6)- (Oy*2)) #0.204975
+
+(mean(votos_Q6)+(Oy*2)) #0.2389047
+
+#[0.204975-0.2389047] existe 95% de confiança de que o intervalo calculado possui a média populacional.
+
+#QuestÃ£o11
+
+#A 
+# H0: Não há associação entre a ideologia da legenda partidária e o posicionamento 
+# do parlamentar quanto a descrimininalização das drogas.
+# H1: Háassociação entre a ideologia da legenda partidária e o posicionamento 
+# do parlamentar quanto a descrimininalização das drogas.
 
 #B
 
+# Erro tipo 1 e Erro tipo 2 que nesse caso o erro de tipo I seria rejeitar a hipótese de que não há associação entre ideologia da legenda partidária
+# e o posicionamento do parlamentar quanto a descriminalização das drogas, quando, na verdade, não há associação. 
+# O erro de tipo II pode seria aceitar que não há associação entre a ideologia partido e o posicionamento do parlamentar quanto a descriminalização 
+# das drogas,quando, na verdade, há assossiação.
+
 #C
 
+favorável <- c(450,150)
 
-#Questão 12
+contrario <- c(100,300)
 
-#Questão 13
+esquerda <- c(450,100)
+
+centro_direita <- c(150,300)
+
+tabela_Q11 <- data.frame(esquerda,centro_direita,row.names = c("favoravel", "contrario"))
+
+chisq.test(tabela_Q11, correct= FALSE)
+
+# Para sustentar a afirmação de que há associação entre as duas variáveiscategóricas do estudo, realizamos o teste Qhi-quadrado. 
+# Neste caso o Quadrado é 242.42,Para df=  1 e p-valor igual a 0,05, temos x²= 3,481 e  que 3,841<242,42. 
+# Desta forma podemos rejeitar a hipótese nula.
+
+#QuestÃ£o 12
+
+Year_Q12 <- c(1964,1966,1968,1970,1972,1974,1976,1978,1980,1982,1984,1986,1988,1990,1992,1994,1996,1998,2000,2002,2004,2006)
+
+Senate <- c(85,88,71,77, 74,85,64,60,55,93,90,75,85,96,83,92,91,90,79,86,96,79)
+
+House <- c(87,88,97,85,94,88,96,94,91,90,95,98,98,96,88,90,94,98,98,96,98,94)
+
+Q_12<- data.frame(Year_Q12,Senate, House)
+
+Antes<- Q_12 %>% filter(Year_Q12 < 1974)
+
+Depois <- Q_12 %>% filter( 1974<=Year_Q12)
+
+# Teste T Senado
+
+t.test(Antes$Senate,Depois$Senate)
+
+# -0.74516
+
+# Teste T Camara
+
+t.test(Antes$House,Depois$House)
+
+#-1.6622
+
+# Os dois testes tiveram P- valor maior que 0,05. Desta forma, não  podemos rejeitar a hipótese nula.
+
+#QuestÃ£o 13
+
+year <- c(1876, 1880,1884,1888,1892,1896,1900,1904,1908,1912,1916,1920,1924,1928,1932)
+
+growth <- c(5.11,3.879,1.589,-5.553, 2.763, -10.024,-1.425,-2.421,-6.281,4.164,2.229,-11.463,-3.872,4.623,-14.586)
+
+vote <- c(48.516,50.22,49.846,50.414,48.268,47.76,53.171,60.006,54.483,54.708,51.682,36.148,58.263,58.756,40.851)
+
+Questao_13 <- data.frame (year, growth, vote)
 
 #A
+# H0: Não há correlação entre o percentual de variação do PIB e o percentual 
+# de votos recebidos pelo candidato do partido incumbente.
+# H1: Há correlação entre o percentual de variação do PIB e o percentual de 
+# votos recebidos pelo candidato do partido incumbente
 
 #B
+cor.test(Questao_13$growth,Questao_13$vote)
+
+# data:  Questao_13$growth and Questao_13$vote
+# t = 2.2546, df = 13, p-value = 0.04205
+# alternative hypothesis: true correlation is not equal to 0
+# 95 percent confidence interval:
+  # 0.02461898 0.81980079
+# sample estimates:
+ # cor 
+# 0.5301953 
+# podemos rejeitar a hipótese nula ao pvalorde 0,05 
+# e assim concluir que mesmo no período mais restrito já era possívelidentificar correlação entre as variáveis.
 
 #C
+
+# Comparando os resultados obtidos, é possível verificar que o aumento no número 
+# de registros na série histórica permitiu rejeitar a hipótese nula com menor pvalor. 
